d6f732fb2023cca6b85d5a3cf97b6e52c12531ef
Shuting Chen <SChen302@outlook.com>
2025-12-05 23:05:21 -0800

    feat: added new feature 'Scroll to Top'

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -236,6 +236,21 @@
     applyBaseTheme(theme);
     restoreCustomThemeIfNeeded(theme);
     createThemeUI(theme);
+
+    const backBtn = document.getElementById("backToTop");
+    if (backBtn) {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 400) {
+          backBtn.classList.add("show");
+        } else {
+          backBtn.classList.remove("show");
+        }
+      });
+
+      backBtn.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      });
+    }
   });
 })();
 
